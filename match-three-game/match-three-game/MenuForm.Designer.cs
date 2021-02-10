4893af3d810bff18fe6d90cd7cd2f6e565aa8be8
Nikita Shpulin <shpulinn@mail.ru>
2021-02-10 22:17:32 +0300

    PlayButton, title added to MenuForm

--- match-three-game/match-three-game/MenuForm.Designer.cs
+++ match-three-game/match-three-game/MenuForm.Designer.cs
@@ -24,7 +24,43 @@ namespace match_three_game {
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent() {
+            this.playButton = new System.Windows.Forms.Button();
+            this.menuLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.playButton.BackColor = System.Drawing.SystemColors.Menu;
+            this.playButton.BackgroundImage = global::match_three_game.Properties.Resources.playBtn;
+            this.playButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.playButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.SandyBrown;
+            this.playButton.FlatAppearance.BorderSize = 5;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.playButton.ForeColor = System.Drawing.Color.DarkOrange;
+            this.playButton.Location = new System.Drawing.Point(350, 300);
+            this.playButton.Margin = new System.Windows.Forms.Padding(0);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(300, 150);
+            this.playButton.TabIndex = 0;
+            this.playButton.UseVisualStyleBackColor = false;
+            // 
+            // menuLabel
+            // 
+            this.menuLabel.AutoSize = true;
+            this.menuLabel.BackColor = System.Drawing.Color.Transparent;
+            this.menuLabel.Font = new System.Drawing.Font("Century Gothic", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuLabel.ForeColor = System.Drawing.Color.DarkOrange;
+            this.menuLabel.Location = new System.Drawing.Point(100, 127);
+            this.menuLabel.Name = "menuLabel";
+            this.menuLabel.Size = new System.Drawing.Size(802, 112);
+            this.menuLabel.TabIndex = 1;
+            this.menuLabel.Text = "Match - 3 Game";
             // 
             // MenuForm
             // 
@@ -32,13 +68,20 @@ namespace match_three_game {
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::match_three_game.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.menuLabel);
+            this.Controls.Add(this.playButton);
+            this.DoubleBuffered = true;
             this.Name = "MenuForm";
             this.Text = "Match3Game";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label menuLabel;
     }
 }
 
