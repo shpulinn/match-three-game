71a78186da7c185b33bb8cb3095fe09fa9569e9a
Nikita Shpulin <shpulinn@mail.ru>
2021-02-10 22:16:43 +0300

    PlayButton added to recources file

--- match-three-game/match-three-game/Properties/Resources.Designer.cs
+++ match-three-game/match-three-game/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace match_three_game.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap playBtn {
+            get {
+                object obj = ResourceManager.GetObject("playBtn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
