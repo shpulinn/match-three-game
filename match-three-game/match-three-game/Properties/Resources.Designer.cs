fe77d675d3391464c4c58d1555e90acce477316f
Nikita Shpulin <shpulinn@mail.ru>
2021-02-13 22:37:32 +0300

    Orange image added to resources file

--- match-three-game/match-three-game/Properties/Resources.Designer.cs
+++ match-three-game/match-three-game/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace match_three_game.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap orange {
+            get {
+                object obj = ResourceManager.GetObject("orange", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playBtn {
             get {
                 object obj = ResourceManager.GetObject("playBtn", resourceCulture);
