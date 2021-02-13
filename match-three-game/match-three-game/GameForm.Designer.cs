80d012eb823fd812abf1bbc587c133147ae28f64
Nikita Shpulin <shpulinn@mail.ru>
2021-02-13 22:38:24 +0300

    now datagridview has 8 image column, some logix realizied
    need to clear code from useless parts, add comments

--- match-three-game/match-three-game/GameForm.Designer.cs
+++ match-three-game/match-three-game/GameForm.Designer.cs
@@ -24,7 +24,108 @@ namespace match_three_game {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(236)))), ((int)(((byte)(197)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(246, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 50;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(490, 490);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(60, 60);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 21;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 21;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 21;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 21;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 21;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 21;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 21;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 21;
             // 
             // GameForm
             // 
@@ -32,12 +133,27 @@ namespace match_three_game {
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::match_three_game.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "GameForm";
             this.Text = "Game";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GameForm_FormClosed);
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewImageColumn Column2;
+        private System.Windows.Forms.DataGridViewImageColumn Column3;
+        private System.Windows.Forms.DataGridViewImageColumn Column4;
+        private System.Windows.Forms.DataGridViewImageColumn Column5;
+        private System.Windows.Forms.DataGridViewImageColumn Column6;
+        private System.Windows.Forms.DataGridViewImageColumn Column7;
+        private System.Windows.Forms.DataGridViewImageColumn Column8;
     }
 }
--- match-three-game/match-three-game/GameForm.cs
+++ match-three-game/match-three-game/GameForm.cs
@@ -10,8 +10,130 @@ using System.Windows.Forms;
 
 namespace match_three_game {
     public partial class GameForm : Form {
+
+        Random rand = new Random();
+        int[,] randInt = new int[8, 8];
+        ImageList imageList = new ImageList();
+
+        public enum Icons {
+             Orange = 1
+        }
+
         public GameForm() {
             InitializeComponent();
+            //dataGridView1.ColumnCount = 8;
+            dataGridView1.RowCount = 8;
+        }
+
+
+        private void GameForm_Load(object sender, EventArgs e) {
+
+            DataGridViewImageColumn imgColumn = new DataGridViewImageColumn() {
+                
+            };
+
+           //for (int i = 0; i < 7; i++) {
+            //dataGridView1.Columns.Add(imgColumn);
+            
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    randInt[i, j] = rand.Next(0, 4);
+                    //randInt[i, j] = 1;
+                }
+            }
+
+            // ======================================================= Просто заполнение числами
+            for (int i = 0; i < dataGridView1.ColumnCount; i++) {
+                for (int j = 0; j < dataGridView1.RowCount; j++) {
+
+                    switch (randInt[i,j]) {
+                        case 1:
+                            ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.orange;
+                        break;
+                    }
+
+                    //if (randInt[i, j].Equals(1)) {
+                    //    ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.orange;
+                    //}
+                    //dataGridView1.Rows[i].Cells[j].Value = randInt[i, j];
+
+                }
+            }
+
+            //for (int i = 0; i < dataGridView1.ColumnCount; i++) {
+            //    for (int j = 0; j < dataGridView1.RowCount; j++) {
+
+            //        if (dataGridView1.Rows[i].Cells[j].Value.ToString() == "1") {
+            //            dataGridView1.Rows[i].Cells[j].Value = Properties.Resources.orange;
+            //        }
+
+            //    }
+            //}
+
+            /*
+            mage image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
+            for (int row = 0; row <= dataGridView1.Rows.Count - 1; row++) {
+                ((DataGridViewImageCell)dataGridView1.Rows[row].Cells[1]).Value = image;
+            }*/
+
+            // ======================================================= Просто заполнение числами
+            //for (int i = 0; i < dataGridView1.ColumnCount; i++) {
+            //    for (int j = 0; j < dataGridView1.RowCount; j++) {
+
+            //        //dataGridView1.Rows.Add($"{i.ToString()}");
+            //        //dataGridView1.Rows[i].Cells[j].Value = i;
+            //    }
+            //}
+
+            // ================================================================== не работает но можно попробовать
+
+            //DataGridViewImageColumn imgColumn = new DataGridViewImageColumn() {
+            //    Name = "Images"
+            //};
+
+            ////dataGridView1.Columns.AddRange(imgColumn);
+
+            //Image image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
+            //imgColumn.Image = image;
+            ////for (int i = 0; i < dataGridView1.Rows.Count; i++) {
+            ////    dataGridView1.Rows[i].Cells["Images"].Value = image;
+            ////}
+            //for (int i = 0; i < dataGridView1.Rows.Count; i++) {
+            //    for (int j = 0; j < dataGridView1.Columns.Count; j++) {
+            //        dataGridView1.Rows[i].Cells[j].Value = image;
+            //        //dataGridView1.Columns.Insert(j, imgColumn);
+            //    }
+            //}
+
+            /*
+            Image image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
+            imageList1.Images.Add(image);
+
+            DataGridViewImageColumn imgCol = new DataGridViewImageColumn();
+            int icIndex = dataGridView1.Columns.Add(imgCol);
+            
+            MessageBox.Show(icIndex.ToString());
+            dataGridView1.Columns[icIndex].Name = "Image";
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+                if (row.Index < imageList1.Images.Count)    // *
+                    row.Cells["Image"].Value = imageList1.Images[row.Index];
+
+            //Image image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
+            for (int i = 0; i < dataGridView1.Rows.Count; i++) {
+                dataGridView1.Rows[i].Cells["Image"].Value = image;
+            }*/
+
+        }
+
+
+
+        private void GameForm_FormClosed(object sender, FormClosedEventArgs e) {
+            //MenuForm menuForm = new MenuForm();
+            //menuForm.Show();
+            //this.Hide();
+            Application.Exit();
         }
     }
 }
