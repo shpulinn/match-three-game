c0e15155fd0a7cc067cde2423f76a13267f51720
Nikita Shpulin <shpulinn@mail.ru>
2021-02-16 00:43:54 +0300

    a lot of work in GameForm class
    1. timer added
    2. timer_tick realized
    3. added button to menu at the end of timer
    4. useless code removed
    5. basics for future methods created

--- match-three-game/match-three-game/GameForm.Designer.cs
+++ match-three-game/match-three-game/GameForm.Designer.cs
@@ -26,8 +26,7 @@ namespace match_three_game {
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.gameGrid = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewImageColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewImageColumn();
@@ -36,19 +35,25 @@ namespace match_three_game {
             this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewImageColumn();
             this.Column8 = new System.Windows.Forms.DataGridViewImageColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabelTitle = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.menuButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.scoreLabelTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gameGrid)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // gameGrid
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.ColumnHeadersVisible = false;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gameGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.gameGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.gameGrid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.gameGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gameGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.gameGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameGrid.ColumnHeadersVisible = false;
+            this.gameGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3,
@@ -61,23 +66,19 @@ namespace match_three_game {
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(236)))), ((int)(((byte)(197)))));
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.SandyBrown;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.Location = new System.Drawing.Point(246, 28);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowTemplate.Height = 50;
-            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.dataGridView1.Size = new System.Drawing.Size(490, 490);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // imageList1
-            // 
-            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
-            this.imageList1.ImageSize = new System.Drawing.Size(60, 60);
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.gameGrid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.gameGrid.Location = new System.Drawing.Point(246, 28);
+            this.gameGrid.MultiSelect = false;
+            this.gameGrid.Name = "gameGrid";
+            this.gameGrid.RowHeadersVisible = false;
+            this.gameGrid.RowTemplate.Height = 50;
+            this.gameGrid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.gameGrid.Size = new System.Drawing.Size(490, 490);
+            this.gameGrid.TabIndex = 0;
+            this.gameGrid.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gameGrid_MouseClick);
             // 
             // Column1
             // 
@@ -127,26 +128,90 @@ namespace match_three_game {
             this.Column8.Name = "Column8";
             this.Column8.Width = 21;
             // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // timeLabelTitle
+            // 
+            this.timeLabelTitle.AutoSize = true;
+            this.timeLabelTitle.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeLabelTitle.Location = new System.Drawing.Point(769, 63);
+            this.timeLabelTitle.Name = "timeLabelTitle";
+            this.timeLabelTitle.Size = new System.Drawing.Size(102, 39);
+            this.timeLabelTitle.TabIndex = 1;
+            this.timeLabelTitle.Text = "Time:";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeLabel.Location = new System.Drawing.Point(769, 115);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 39);
+            this.timeLabel.TabIndex = 2;
+            // 
+            // menuButton
+            // 
+            this.menuButton.BackgroundImage = global::match_three_game.Properties.Resources.playBtn;
+            this.menuButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuButton.Location = new System.Drawing.Point(350, 200);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(300, 150);
+            this.menuButton.TabIndex = 3;
+            this.menuButton.UseVisualStyleBackColor = true;
+            this.menuButton.Visible = false;
+            this.menuButton.Click += new System.EventHandler(this.MenuButton_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(55, 115);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(0, 39);
+            this.scoreLabel.TabIndex = 5;
+            // 
+            // scoreLabelTitle
+            // 
+            this.scoreLabelTitle.AutoSize = true;
+            this.scoreLabelTitle.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabelTitle.Location = new System.Drawing.Point(55, 63);
+            this.scoreLabelTitle.Name = "scoreLabelTitle";
+            this.scoreLabelTitle.Size = new System.Drawing.Size(115, 39);
+            this.scoreLabelTitle.TabIndex = 4;
+            this.scoreLabelTitle.Text = "Score:";
+            // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::match_three_game.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(984, 561);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.scoreLabelTitle);
+            this.Controls.Add(this.menuButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.timeLabelTitle);
+            this.Controls.Add(this.gameGrid);
             this.Name = "GameForm";
             this.Text = "Game";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GameForm_FormClosed);
             this.Load += new System.EventHandler(this.GameForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.DataGridView gameGrid;
         private System.Windows.Forms.DataGridViewImageColumn Column1;
         private System.Windows.Forms.DataGridViewImageColumn Column2;
         private System.Windows.Forms.DataGridViewImageColumn Column3;
@@ -155,5 +220,11 @@ namespace match_three_game {
         private System.Windows.Forms.DataGridViewImageColumn Column6;
         private System.Windows.Forms.DataGridViewImageColumn Column7;
         private System.Windows.Forms.DataGridViewImageColumn Column8;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label timeLabelTitle;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label scoreLabelTitle;
     }
 }
--- match-three-game/match-three-game/GameForm.cs
+++ match-three-game/match-three-game/GameForm.cs
@@ -13,143 +13,139 @@ namespace match_three_game {
 
         Random rand = new Random();
         int[,] randInt = new int[8, 8];
-        ImageList imageList = new ImageList();
-
-        public enum Icons {
-             Orange = 1
-        }
+        int gameTime = 7;
+        int score = 0;
+        bool inGame = true;
 
         public GameForm() {
             InitializeComponent();
-            //dataGridView1.ColumnCount = 8;
-            dataGridView1.RowCount = 8;
+            gameGrid.RowCount = 8;
         }
-
 
         private void GameForm_Load(object sender, EventArgs e) {
 
-            DataGridViewImageColumn imgColumn = new DataGridViewImageColumn() {
-                
-            };
+            gameGrid.CurrentCell = null;
+            FillBoard();
+            gameTimer.Start();
+            // while inGame///
+            //while (inGame) {
+            //    PlayerMove();
+            //}
 
-           //for (int i = 0; i < 7; i++) {
-            //dataGridView1.Columns.Add(imgColumn);
-            
+        }
 
+        private void PlayerMove() {
+            int[,] copyArr = new int[8, 8]; // copy arr??
+            string msg = String.Format("Row: {0}, Column: {1}",
+                                        gameGrid.CurrentCell.RowIndex,
+                                        gameGrid.CurrentCell.ColumnIndex);
+            MessageBox.Show(msg, "Current Cell");
+            var curCell = gameGrid.CurrentCell;
+            for (int i = 0; i < randInt.Length; i++) {
+                for (int j = 0; j < randInt.Length; j++) {
+                    if (copyArr[i,j] != randInt[i,j]) {
+
+                    }
+                    copyArr[i, j] = randInt[i, j];
+                }
+            }
+
+        }
+
+        private void UpdateGrid() {
+            for (int i = 0; i < randInt.Length; i++) {
+                for (int j = 0; j < randInt.Length; j++) {
+                    // костылЬ!!
+                    switch (randInt[i, j]) {
+                        case 0: // banana
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.banana;
+                        break;
+
+                        case 1: //orange
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.orange;
+                        break;
+
+                        case 2: //apple
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.apple;
+                        break;
+
+                        case 3: //grape
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.grape;
+                        break;
+
+                        case 4: //pear
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.pear;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        private void FillBoard() {
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-                    randInt[i, j] = rand.Next(0, 5);
+                    randInt[i, j] = rand.Next(0, 5); // заполнение массива числами от 0 до 4
                     //randInt[i, j] = 1;
                 }
             }
 
-            // ======================================================= Просто заполнение числами
-            for (int i = 0; i < dataGridView1.ColumnCount; i++) {
-                for (int j = 0; j < dataGridView1.RowCount; j++) {
-
-                    switch (randInt[i,j]) {
-                        case 0:
-                            ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.banana;
-                        break;
-                            
-                        case 1:
-                            ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.orange;
+            // ======================================================= добавление в DataGridView иконок в зависимости от случ. числа
+            for (int i = 0; i < gameGrid.ColumnCount; i++) {
+                for (int j = 0; j < gameGrid.RowCount; j++) {
+                    switch (randInt[i, j]) {
+                        case 0: // banana
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.banana;
                         break;
 
-                        case 2:
-                            ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.apple;
+                        case 1: //orange
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.orange;
                         break;
 
-                        case 3:
-                        ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.grape;
+                        case 2: //apple
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.apple;
                         break;
 
-                        case 4:
-                        ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.pear;
+                        case 3: //grape
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.grape;
+                        break;
+
+                        case 4: //pear
+                        ((DataGridViewImageCell)gameGrid.Rows[i].Cells[j]).Value = Properties.Resources.pear;
                         break;
                     }
-
-                    //if (randInt[i, j].Equals(1)) {
-                    //    ((DataGridViewImageCell)dataGridView1.Rows[i].Cells[j]).Value = Properties.Resources.orange;
-                    //}
-                    //dataGridView1.Rows[i].Cells[j].Value = randInt[i, j];
-
                 }
             }
-
-            //for (int i = 0; i < dataGridView1.ColumnCount; i++) {
-            //    for (int j = 0; j < dataGridView1.RowCount; j++) {
-
-            //        if (dataGridView1.Rows[i].Cells[j].Value.ToString() == "1") {
-            //            dataGridView1.Rows[i].Cells[j].Value = Properties.Resources.orange;
-            //        }
-
-            //    }
-            //}
-
-            /*
-            mage image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
-            for (int row = 0; row <= dataGridView1.Rows.Count - 1; row++) {
-                ((DataGridViewImageCell)dataGridView1.Rows[row].Cells[1]).Value = image;
-            }*/
-
-            // ======================================================= Просто заполнение числами
-            //for (int i = 0; i < dataGridView1.ColumnCount; i++) {
-            //    for (int j = 0; j < dataGridView1.RowCount; j++) {
-
-            //        //dataGridView1.Rows.Add($"{i.ToString()}");
-            //        //dataGridView1.Rows[i].Cells[j].Value = i;
-            //    }
-            //}
-
-            // ================================================================== не работает но можно попробовать
-
-            //DataGridViewImageColumn imgColumn = new DataGridViewImageColumn() {
-            //    Name = "Images"
-            //};
-
-            ////dataGridView1.Columns.AddRange(imgColumn);
-
-            //Image image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
-            //imgColumn.Image = image;
-            ////for (int i = 0; i < dataGridView1.Rows.Count; i++) {
-            ////    dataGridView1.Rows[i].Cells["Images"].Value = image;
-            ////}
-            //for (int i = 0; i < dataGridView1.Rows.Count; i++) {
-            //    for (int j = 0; j < dataGridView1.Columns.Count; j++) {
-            //        dataGridView1.Rows[i].Cells[j].Value = image;
-            //        //dataGridView1.Columns.Insert(j, imgColumn);
-            //    }
-            //}
-
-            /*
-            Image image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
-            imageList1.Images.Add(image);
-
-            DataGridViewImageColumn imgCol = new DataGridViewImageColumn();
-            int icIndex = dataGridView1.Columns.Add(imgCol);
-            
-            MessageBox.Show(icIndex.ToString());
-            dataGridView1.Columns[icIndex].Name = "Image";
-
-            foreach (DataGridViewRow row in dataGridView1.Rows)
-                if (row.Index < imageList1.Images.Count)    // *
-                    row.Cells["Image"].Value = imageList1.Images[row.Index];
-
-            //Image image = new Bitmap("C:\\Users\\shpul\\Desktop\\orange.png");
-            for (int i = 0; i < dataGridView1.Rows.Count; i++) {
-                dataGridView1.Rows[i].Cells["Image"].Value = image;
-            }*/
-
         }
 
-
-
         private void GameForm_FormClosed(object sender, FormClosedEventArgs e) {
-            //MenuForm menuForm = new MenuForm();
-            //menuForm.Show();
-            //this.Hide();
             Application.Exit();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e) {
+            if (gameTime > 0) {
+                gameTime -= 1;
+                timeLabel.Text = gameTime.ToString();
+            }
+            else {
+                gameTimer.Stop();
+                timeLabel.Text = "Time's up!";
+                menuButton.Visible = true;
+                inGame = false;
+            }
+        }
+
+        private void MenuButton_Click(object sender, EventArgs e) {
+            MenuForm menuForm = new MenuForm();
+            menuForm.Show();
+            this.Hide();
+        }
+
+        private void gameGrid_MouseClick(object sender, MouseEventArgs e) {
+            string msg = String.Format("Row: {0}, Column: {1}",
+        gameGrid.CurrentCell.RowIndex,
+        gameGrid.CurrentCell.ColumnIndex);
+            MessageBox.Show(msg, "Current Cell");
         }
     }
 }
